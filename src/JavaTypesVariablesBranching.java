e6ba082bc31c59945e6c49c0210f155c88a7c551
akinsoug <akinsoug@uncc.edu>
2020-11-09 20:42:24 -0500

    first cmit 1

---
+++ src/JavaTypesVariablesBranching.java
@@ -0,0 +1,16 @@
+
+public class JavaTypesVariablesBranching {
+	int a ;
+	
+	public static void main(String[] args) {
+		
+		int b;
+		int c = 10;
+		
+//		System.out.println(a); // a must be ststic
+//		System.out.println(b); // b must be initialized
+		System.out.println(c);
+		
+	}
+
+}
